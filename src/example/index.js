9b8b94d3447e205eca8f57e244469c092f13f1ff
thinkty <thinkty@naver.com>
2020-12-17 21:40:17 +0900

    Moved experimental messages to example dir

--- src/example/index.js
+++ src/example/index.js
@@ -1,5 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactChat from '../index';
+import Message from '../lib/Message';
 
-ReactDOM.render(<ReactChat/>, document.getElementById('root'));
+const messages = [];
+messages.push(new Message({
+  isOwner: false,
+  sender: 'Mark',
+  message: 'lol',
+  messageType: messageTypes.text,
+}));
+messages.push(new Message({
+  isOwner: false,
+  sender: 'Mark Hamill',
+  message: 'Hello World',
+  messageType: messageTypes.text,
+}));
+messages.push(new Message({
+  isOwner: true,
+  sender: 'Spongebob',
+  message: '너와 함께한 시간 모두 눈부셨다. 날이 좋아서 날이 좋지 않아서 날이 적당해서 모든 날이 좋았다. 그리고 무슨 일이 벌어져도 네 잘못이 아니다.',
+  messageType: messageTypes.text,
+}));
+messages.push(new Message({
+  isOwner: false,
+  sender: 'Bob the destroyer of dimensions and the lord of the galaxy',
+  message: 'Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not.',
+  messageType: messageTypes.text,
+}));
+messages.push(new Message({
+  isOwner: true,
+  sender: 'Empty Message',
+  message: '',
+  messageType: messageTypes.text,
+}));
+
+ReactDOM.render(
+  <div>
+    <p>React Chat</p>
+    <ReactChat
+    
+    />
+  </div>
+, document.getElementById('root'));

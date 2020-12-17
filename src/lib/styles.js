09067f2387bb5b1ec6c8eb563a589754345737f6
thinkty <thinkty@naver.com>
2020-12-17 21:39:36 +0900

    Added styles module

---
+++ src/lib/styles.js
@@ -0,0 +1,42 @@
+/**
+ * This module contains style properties for various components
+ */
+
+// Default primary message style for the UserMessage component
+export const defaultPrimaryStyle = {
+  color: '#fff',
+  backgroundColor: '#7070db',
+  border: `thin solid #7070db`,
+  borderRadius: '10px',
+  padding: '6px',
+  margin: '3px',
+  display: 'inline-block',
+  fontFamily: 'Helvetica, sans-serif',
+  overflowWrap: 'break-word',
+  maxWidth: '200px',
+};
+
+// Default secondary message style for the UserMessage component
+export const defaultSecondaryStyle = {
+  color: '#000',
+  backgroundColor: '#DCDCDC',
+  border: 'thin solid #DCDCDC',
+  borderRadius: '10px',
+  padding: '6px',
+  margin: '3px',
+  display: 'inline-block',
+  fontFamily: 'Helvetica, sans-serif',
+  overflowWrap: 'break-word',
+  maxWidth: '200px',
+};
+
+// Default sender name field style for the UserMessage component
+export const defaultSenderStyle = {
+  color: '#808080',
+  fontFamily: 'Helvetica, sans-serif',
+  textOverflow: 'ellipsis',
+  whiteSpace: 'nowrap',
+  overflow: 'hidden',
+  margin: '5px 10px 0px 10px',
+  maxWidth: '200px',
+};

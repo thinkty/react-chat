316a9904161c694947d3c52469bb4c495b9feded
thinkty <thinkty@naver.com>
2020-12-27 15:55:29 +0900

    Modified webpack configurations to exclude example from production

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const common = require('./webpack.common');
 
@@ -15,9 +14,5 @@ module.exports = merge(common, {
   },
   plugins: [
     new CleanWebpackPlugin(),
-    new HtmlWebPackPlugin({
-      template: path.resolve(__dirname, 'src/example/index.html'),
-      filename: 'index.html',
-    })
   ],
 });

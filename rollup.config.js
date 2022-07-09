08467e7f8b386a852640c0c3aa64f2798db0c629
thinkty <thinkty@naver.com>
2022-07-09 23:35:40 +0900

    Using rollup instead of webpack

---
+++ rollup.config.js
@@ -0,0 +1,41 @@
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+import dts from 'rollup-plugin-dts';
+
+import packageJson from './package.json';
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    external: ["react", "react-dom", "uuid"],
+    plugins: [
+      peerDepsExternal(),
+      nodeResolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
+      terser(),
+    ],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [
+      dts(),
+    ]
+  }
+]
